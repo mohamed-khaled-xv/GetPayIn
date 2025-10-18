b254803c2ca99360204d4d090b446ea4b3c6920b
mohamed-khaled-xv <mohamedkhaledxv8@gmail.com>
2025-10-18 17:22:27 +0300

    removed the unused shared components

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,13 +1,8 @@
-export { Button } from './Button';
-export { Card } from './Card';
 export { CustomDeleteAlert } from './CustomDeleteAlert';
 export { createDeleteHandler, createOptimisticDeleteHandler, showDeleteAlert } from './DeleteAlert';
 export { DeleteAlertProvider } from './DeleteAlertProvider';
 export { createFakeDeleteHandler } from './FakeDeleteHandler';
 export { Input } from './Input';
-export { createLocalDeleteHandler } from './LocalDeleteHandler';
 export { default as LockOverlay } from './LockOverlay';
-export { createNativeOptimisticDeleteHandler } from './NativeDeleteAlert';
 export { default as SplashScreen } from './SplashScreen';
-export { createTestDeleteHandler } from './TestDeleteHandler';
 
